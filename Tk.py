7454bf6ee815ef345fefbf7d387adcc7094a3a42
Feo <maks-pavell@rambler.ru>
2019-03-08 19:01:38 +0300

    1) Update GUI 2) Update README

--- Tk.py
+++ Tk.py
@@ -1,8 +1,25 @@
 import tkinter
 
 
-def main():
+root = tkinter.Tk()
 
-    root = tkinter.Tk()
+e = tkinter.Entry(root, width=20)
+b = tkinter.Button(root, text="Ask")
+l = tkinter.Label(root, bg='black', fg='white', width=20)
+
+
+def processing(event):
+    s = e.get()
+    l['text'] = ' '.join(s)
+
+
+b.bind('<Button-1>', processing)
+
+e.pack()
+b.pack()
+l.pack()
+
+
+def main():
 
     root.mainloop()
